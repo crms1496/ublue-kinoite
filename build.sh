9b1e99bdb4eb220cd03dd9c8d2ca898b3d65f92d
crms1496 <crms1496@gmail.com>
2024-06-04 10:45:26 -0400

    Add refresh-md -f to build process

--- build.sh
+++ build.sh
@@ -13,10 +13,12 @@ curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedor
 
 ### Install packages
 
+# Refresh repository metadata
+rpm-ostree refresh-md --force
+
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
 rpm-ostree install byobu duperemove fish mosh podman-compose tailscale virt-manager \
     keyd ibm-plex-fonts-all adw-gtk3-theme bubblejail gamemode goverlay mangohud protontricks steam vkBasalt
