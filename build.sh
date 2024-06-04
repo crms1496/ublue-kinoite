b15b4e8f1e8477aafb01eab75697f661d3f95bd9
crms1496 <crms1496@gmail.com>
2024-06-04 10:50:08 -0400

    Revert refresh-md -f
    rpm-ostree reports that refresh-md does not work on systems not booted via libostree

--- build.sh
+++ build.sh
@@ -13,9 +13,6 @@ curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedor
 
 ### Install packages
 
-# Refresh repository metadata
-rpm-ostree refresh-md --force
-
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
